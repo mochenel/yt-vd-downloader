8572cd2fbd4f30ff5837d990159af0480d9bd7f9
mochenel <freddy980404@gmail.com>
2022-04-15 18:07:19 +0200

    connect to heroku host

--- src/services/Video.js
+++ src/services/Video.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const host = "http://localhost:9000";
+const host = "https://yt-vd-downloader-nodejs.herokuapp.com";
 async function getVideoDetails(videoURL) {
     const END_POINT = `${host}/getVideoDetails?url=${videoURL}`;
 
