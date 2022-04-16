c3563ab5127e872d6011f2e1dd63f38b6b36cd32
mochenel <freddy980404@gmail.com>
2022-04-16 12:39:41 +0200

    add copy link instruction

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export default function Header() {
   return (
-    <h3 style={{textAlign:"center", marginBottom:"50px"}} > Download youtube video </h3>
+    <div>
+      <h3 style={{textAlign:"center"}} > Download youtube video </h3>
+       <div style={{marginTop: "30px",display: "flex", justifyContent: "center",flexDirection:"column",alignItems: "center", width: "100%"}}>
+         <strong>How to copy youtube video url</strong>
+         <img src="copylink.png" alt="" width="30%"  />
+       </div>
+    </div>
+    
   )
 }
